ca1aec6095f8125641910f1c0a41bef2585c968d
Milan Malfait <milan.malfait94@gmail.com>
2021-08-17 19:05:48 +0200

    Add example datasets

---
+++ R/data.R
@@ -0,0 +1,38 @@
+#' SwissProt data
+#'
+#' TODO: describe data briefly...
+#'
+#' @format An mzID object.
+#' TODO: add some more details...
+#'
+#' @usage data(ModSwiss)
+#'
+#' @source
+#' TODO: add source of dataset
+"ModSwiss"
+
+#' SwissProt X!Tandem data
+#'
+#' TODO: describe data briefly...
+#'
+#' @format An mzID object.
+#' TODO: add some more details...
+#'
+#' @usage data(ModSwissXT)
+#'
+#' @source
+#' TODO: add source of dataset
+"ModSwissXT"
+
+#' SwissProt data with different search engines
+#'
+#' TODO: describe data briefly...
+#'
+#' @format An mzID object.
+#' TODO: add some more details...
+#'
+#' @usage data(pyroSwissprot)
+#'
+#' @source
+#' TODO: add source of dataset
+"pyroSwissprot"
