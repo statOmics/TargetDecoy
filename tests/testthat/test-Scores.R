b414461fe63edf9efd0494368715a99e84ec6ca3
Milan Malfait <milan.malfait94@gmail.com>
2021-09-01 19:40:22 +0200

    Add tests for `createPPlotScores()`

---
+++ tests/testthat/test-Scores.R
@@ -0,0 +1,28 @@
+## Use one of the example files in the mzID package
+exampleFile <- system.file("extdata", "55merge_tandem.mzid", package = "mzID")
+mzIDexample <- mzID::mzID(exampleFile)
+
+test_that("createPPlotScores() works", {
+    out <- createPPlotScores(mzIDexample,
+        scores = c("x\\!tandem:hyperscore", "x\\!tandem:expect"),
+        decoy = "isdecoy", log10 = TRUE
+    )
+
+    expect_type(out, "list")
+    expect_length(out, 2)
+})
+
+test_that("createPPLotScores() fails for incompatible arg lengths", {
+    expect_error(
+        createPPlotScores(mzIDexample,
+            scores = c("x\\!tandem:hyperscore", "x\\!tandem:expect"),
+            decoy = "isdecoy", log10 = c(TRUE, FALSE, TRUE)
+        )
+    )
+    expect_error(
+        createPPlotScores(mzIDexample,
+            scores = c("x\\!tandem:hyperscore", "x\\!tandem:expect"),
+            decoy = rep("isdecoy", 3), log10 = TRUE
+        )
+    )
+})
