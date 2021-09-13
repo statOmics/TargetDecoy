1ffbd03be383190e92e946ab89c2e57d7a9d6284
ElkeDebrie <ElkeDebrie@users.noreply.github.com>
2021-09-13 11:14:50 +0200

    Add data description

--- R/data.R
+++ R/data.R
@@ -1,5 +1,13 @@
-#' SwissProt data
+#' Swiss-Prot MS-GF+ data
 #'
+#' @description 
+#' Data from a Pyrococcus furiosis sample run on a LTQ-Orbitrap Velos mass spectrometer. 
+#' The data can be found in the PRIDE repository with identifier PXD001077. 
+#' The Pyrococcus furiosis reference proteome fasta files were downloaded from 
+#' UniProtKB/Swiss-Prot on April 22, 2016. 
+#' The Pyrococcus data was searched against all Pyrococcus proteins 
+#' with MS-GF+ search engines using either the reference proteome 
+#' from UniProtKB/Swiss-Prot.
 #' 
 #'
 #' @format An mzID object.
@@ -11,9 +19,16 @@
 #' TODO: add source of dataset
 "ModSwiss"
 
-#' SwissProt X!Tandem data
+#' Swiss-Prot X!Tandem data
 #'
-#' TODO: describe data briefly...
+#' @description 
+#' Data from a Pyrococcus furiosis sample run on a LTQ-Orbitrap Velos mass spectrometer. 
+#' The data can be found in the PRIDE repository with identifier PXD001077. 
+#' The Pyrococcus furiosis reference proteome fasta files were downloaded from 
+#' UniProtKB/Swiss-Prot on April 22, 2016. 
+#' The Pyrococcus data was searched against all Pyrococcus proteins 
+#' with X!Tandem search engines using either the reference proteome 
+#' from UniProtKB/Swiss-Prot.
 #'
 #' @format An mzID object.
 #' TODO: add some more details...
