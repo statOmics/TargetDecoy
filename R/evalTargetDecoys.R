a8ddafb4c35c8e1afb7beeb7d6558365693b57b5
Milan Malfait <milan.malfait94@gmail.com>
2021-10-18 21:05:25 +0200

    Clean up `decoyScoreTable()`
    * Remove Shiny functionality
    * Combine similar error messages
    * Remove default values for `decoy=` and `score=`

--- R/evalTargetDecoys.R
+++ R/evalTargetDecoys.R
@@ -1,8 +1,6 @@
 #' Prepare score table for decoys
 #'
-#' Takes an input object and returns a score table for the decoys. If one of the
-#' arguments besides `object` is missing, will open a Shiny app to interactively
-#' select the variables.
+#' Takes an input object and returns a score table for the decoys.
 #'
 #' @param object A `data.frame`, \linkS4class{mzID} or \linkS4class{mzRident} object.
 #' @param decoy `character`, name of the variable that indicates if the peptide
@@ -18,31 +16,13 @@
 #'
 #' @author Elke Debrie, Lieven Clement
 #' @keywords internal
-decoyScoreTable <- function(object, decoy = NULL, score = NULL, log10 = TRUE) {
+decoyScoreTable <- function(object, decoy, score, log10 = TRUE) {
     df <- .getDF(object)
 
-    if (is.null(decoy) || is.null(score) || is.null(log10)) {
-        if (missing(log10)) log10 <- TRUE
-        out <- .select(df, decoy, score, log10)
-        decoy <- out$selDecoy # categorical
-        score <- out$selScore # continu
-        log10 <- out$log
-    }
-
-    ###############
-    # VERVANGEN?
-
-    if (!(score %in% colnames(df)) | !(decoy %in% colnames(df))) {
-        stop("Column(s) not found:", call. = FALSE)
-    }
-
-    ###############
-
-    if (!(decoy %in% colnames(df))) {
-       stop("`decoy = '", decoy, "'` not found in input object.", call. = FALSE)
-    }
-    if (!(score %in% colnames(df))) {
-       stop("`score = '", score, "'` not found in input object.", call. = FALSE)
+    is_missing <- !(c(decoy, score) %in% colnames(df))
+    if (any(is_missing)) {
+        missing <- paste(c(decoy, score)[is_missing], collapse = ", ")
+        stop("Variable(s) not found: ", missing, call. = FALSE)
     }
 
     table <- df[, c(decoy, score)]
