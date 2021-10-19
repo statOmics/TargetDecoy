2ac88620d3a14df01d72b6d2a8c91792a530062e
Milan Malfait <milan.malfait94@gmail.com>
2021-10-19 16:20:51 +0200

    Update data documentation

--- R/data.R
+++ R/data.R
@@ -1,40 +1,31 @@
 #' Swiss-Prot MS-GF+ data
 #'
-#' @description 
-#' Data from a Pyrococcus furiosis sample run on a LTQ-Orbitrap Velos mass spectrometer. 
-#' The data can be found in the PRIDE repository with identifier PXD001077. 
-#' The Pyrococcus furiosis reference proteome fasta files were downloaded from 
-#' UniProtKB/Swiss-Prot on April 22, 2016. 
-#' The Pyrococcus data was searched against all Pyrococcus proteins 
-#' with MS-GF+ search engines using the reference proteome 
+#' @description
+#' Data from a Pyrococcus furiosis sample run on a LTQ-Orbitrap Velos mass spectrometer.
+#' The data can be found in the PRIDE repository with identifier PXD001077.
+#' The Pyrococcus furiosis reference proteome fasta files were downloaded from
+#' UniProtKB/Swiss-Prot on April 22, 2016.
+#' The Pyrococcus data was searched against all Pyrococcus proteins
+#' with MS-GF+ search engines using the reference proteome
 #' from UniProtKB/Swiss-Prot.
-#' 
 #'
-#' @format An mzID object.
-#' TODO: add some more details...
+#' @format An \linkS4class{mzID} object.
 #'
 #' @usage data(ModSwiss)
-#'
-#' @source
-#' TODO: add source of dataset
 "ModSwiss"
 
 #' Swiss-Prot X!Tandem data
 #'
-#' @description 
-#' Data from a Pyrococcus furiosis sample run on a LTQ-Orbitrap Velos mass spectrometer. 
-#' The data can be found in the PRIDE repository with identifier PXD001077. 
-#' The Pyrococcus furiosis reference proteome fasta files were downloaded from 
-#' UniProtKB/Swiss-Prot on April 22, 2016. 
-#' The Pyrococcus data was searched against all Pyrococcus proteins 
-#' with a combined search (omssa, X!Tandem and MS-GF+) using the reference proteome 
+#' @description
+#' Data from a Pyrococcus furiosis sample run on a LTQ-Orbitrap Velos mass spectrometer.
+#' The data can be found in the PRIDE repository with identifier PXD001077.
+#' The Pyrococcus furiosis reference proteome fasta files were downloaded from
+#' UniProtKB/Swiss-Prot on April 22, 2016.
+#' The Pyrococcus data was searched against all Pyrococcus proteins
+#' with a combined search (omssa, X!Tandem and MS-GF+) using the reference proteome
 #' from UniProtKB/Swiss-Prot.
 #'
-#' @format An mzID object.
-#' TODO: add some more details...
+#' @format An \linkS4class{mzID} object.
 #'
 #' @usage data(ModSwissXT)
-#'
-#' @source
-#' TODO: add source of dataset
 "ModSwissXT"
